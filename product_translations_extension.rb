0540f953f23a40b295cee81fb98d29720ffa1fba
Remy Giard <remy@remygiard.com>
2009-11-14 15:12:42 -0500

    basic creation of the translation bar

--- product_translations_extension.rb
+++ product_translations_extension.rb
@@ -6,8 +6,7 @@ class ProductTranslationsExtension < Spree::Extension
   description "Makes product details, properties, prototypes and taxonomies translatable using the Globalize 2 plugin"
   
   def activate
-    # add content translations using Globalize 2's helpers
-    
+
     Product.class_eval do
       translates :name, :description, :meta_description, :meta_keywords
     end
